c583b420dae5e50c63c5aa25d6d57b3f2ebb72a6
majimajico55 <outlawstar525@gmail.com>
2022-05-12 22:18:46 +0900

    新規作成

---
+++ speech.js
@@ -0,0 +1,15 @@
+// 再生ボタン
+const start = document.querySelector('#btnStart');
+
+// 再生に関する設定
+const speechSet = new SpeechSynthesisUtterance();
+speechSet.text = "たたいて、かぶって、ジャン・ケン・ポン";
+speechSet.lang = 'ja-JP';
+
+// ボタンクリックイベント
+start.addEventListener('click', function(){
+  // 喋るスピードをランダムに
+  speechSet.rate = Number((Math.random() * 10).toFixed(2));
+  // 再生
+  speechSynthesis.speak(speechSet);
+}, false);
